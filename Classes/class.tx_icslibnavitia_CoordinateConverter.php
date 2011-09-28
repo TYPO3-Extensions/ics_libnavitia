403006e6098a831dd4ff5e4d886eec7221259e1c
mygoddess <mygoddess@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-28 20:39:57 +0000

    Corrected coordinate convertion initial values. Added comments for missing convertion steps.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_libnavitia/trunk@52542 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/class.tx_icslibnavitia_CoordinateConverter.php
+++ Classes/class.tx_icslibnavitia_CoordinateConverter.php
@@ -2,36 +2,42 @@
 
 // http://professionnels.ign.fr/DISPLAY/000/526/701/5267019/NTG_71.pdf paramètres.
 // http://geodesie.ign.fr/contenu/fichiers/documentation/pedagogiques/transfo.pdf p12 paramètres.
+// http://www.forumsig.org/showthread.php?p=64050#post64050
 class tx_icslibnavitia_CoordinateConverter {
 
-	private static $n = 0.7289127966; // Exposant de la projection.
-	private static $c = 11745793.39; // Constante de la projection.
-	private static $Xs = 600000.0; // Coordonnées en projection du pôle.
-	private static $Ys = 8199695.768; // Coordonnées en projection du pôle.
+	private static $n = 0.72896862742141; // Exposant de la projection. n=sin(phi0)
+	private static $c = 11745793.39; // Constante de la projection. c=s*N(phi0,a,e)*cotan(phi0)*exp(n*LatIso(phi0,e)) avec N(phi,a,e)=a/sqrt(1-e²*sin(phi)²) et LatIso(phi,e)=ln(tan(PI/4+phi/2)*((1-e*sin(phi))/(1+e*sin(phi)))^(e/2)) // N: grande normale
+	private static $Xs = 600000.0; // Coordonnées en projection du pôle. Xs=X0
+	private static $Ys = 8199695.768; // Coordonnées en projection du pôle. Ys=Y0+s*N(phi0, a, e)*cotan(phi0)
 	private static $lambda0; // = deg2rad((14.025 / 60 + 20) / 60 + 2); // Longitude du méridien de paris par rapport au méridien d'origine. (rad)
-	private static $e = 0.08248325676; // Première excentricité de l'ellipsoïde.
+	private static $e = 0.0824832567634177; // Première excentricité de l'ellipsoïde. ((SQRT(.006803487646))) e=sqrt((a²-b²)/a²)
 	
 	private static $a = 6378249.2; // Demi grand axe. (m)
+	private static $b = 6356515; // Demi petit axe. (m) b=a*(1-f)
 	private static $f; // = 1 / 293.466021; // Applatissement.
-	// Lambert II Carto (Centre) 	52 grades 	45°53’56,108” 	47°41’45,652” 	600 000 m 	2 200 000 m (http://fr.wikipedia.org/wiki/Projection_de_Lambert)
 	private static $phi0; // = deg2rad(48 / 60 + 46); // Latitude origine. (rad)
 	private static $phi1; // = deg2rad((56.108 / 60 + 53) / 60 + 45); // Latitude du premier parallèle automécoïque. (rad)
 	private static $phi2; // = deg2rad((45.652 / 60 + 41) / 60 + 47); // Latitude du second parallèle automécoïque. (rad)
 	private static $X0 = 600000; // Translation X. (m) (E0)
 	private static $Y0 = 2200000; // Translation Y. (m) (N0)
-	private static $s = 0.99987742; // Facteur d'échelle.
+	private static $s = 0.99987742; // Facteur d'échelle. (k0)
 	
 	public static function init() {
 		self::$lambda0 = deg2rad((14.025 / 60 + 20) / 60 + 2);
-		self::$f = 1 / 293.466021;
+		self::$f = 1 / 293.466021293627;
 		self::$phi0 = deg2rad(48 / 60 + 46);
 		self::$phi1 = deg2rad((56.108 / 60 + 53) / 60 + 45);
 		self::$phi2 = deg2rad((45.652 / 60 + 41) / 60 + 47);
 	}
-	// θλρπφ₀₁
+	
 	public static function convertfromWGS84($lat, $lng) {
 		$phi = deg2rad($lat);
 		$lambda = deg2rad($lng);
+		
+		// list($X, $Y, $Z) = convertToWGS84Cartesian($phi, $lambda);
+		// list($X, $Y, $Z) = translateForNTF($X, $Y, $Z);
+		// list($phi, $lambda) = convertFromNTFCartesian($X, $Y, $Z);
+		// list($X, $Y) = convertToLambertIIExtended($phi, $lambda);
 		
 		$L = log(tan((M_PI / 4) + ($phi / 2))) * pow((1 - self::$e * sin($phi)) / (1 + self::$e * sin($phi)), self::$e / 2);
 		// $L = .5 * log((1 + sin($lat)) / (1 - sin($lat))) - self::$e * .5 * log((1 + self::$e * sin($lat)) / (1 - self::$e * sin($lat)));
@@ -43,6 +49,11 @@ class tx_icslibnavitia_CoordinateConverter {
 	}
 	
 	public static function convertToWGS84($X, $Y) {
+		// list($phi, $lambda) = convertFromLambertIIExtended($X, $Y);
+		// list($X, $Y, $Z) = convertToNTFCartesian($phi, $lambda);
+		// list($X, $Y, $Z) = translateForWGS84($X, $Y, $Z);
+		// list($phi, $lambda) = convertFromWGS84Cartesian($X, $Y, $Z);
+	
 		$dX = $X - self::$Xs;
 		$dY = $Y - self::$Ys;
 		$R = sqrt($dX * $dX + $dY * $dY);
