9d0313c865abf3717d3593744be13e843716359a
mygoddess <mygoddess@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-19 15:34:28 +0000

    Added APIService::getStopAreasByLineCode. Bug Fix.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_libnavitia/trunk@76635 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Services/class.tx_icslibnavitia_APIService.php
+++ Classes/Services/class.tx_icslibnavitia_APIService.php
@@ -556,6 +556,52 @@ class tx_icslibnavitia_APIService {
 		}
 		return $list;
 	}
+
+	/**
+	 * Query the PTReferential API function for StopAreaList for a single line.
+	 *
+	 * @param string $lineExternalCode The unique identifier of the line.
+	 * @return tx_icslibnavitia_INodeList The list of line attached stop areas. Each element is a {@link tx_icslibnavitia_StopArea}.
+	 */
+	public function getStopAreasByLineCode($lineExternalCode) {
+		$params = array();
+		$params['RequestedType'] = 'StopAreaList';
+		if ($lineExternalCode !== null) {
+			$params['LineExternalCode'] = $lineExternalCode;
+		}
+		$xml = $this->CallAPI('PTReferential', $params);
+		if (!$xml) {
+			tx_icslibnavitia_Debug::warning('Failed to call PTReferential API; See devlog for additional information');
+			return null;
+		}
+		$reader = new XMLReader();
+		$reader->XML($xml);
+		if (!$this->XMLMoveToRootElement($reader, 'ActionStopAreaList')) {
+			tx_icslibnavitia_Debug::warning('Invalid response from PTReferential API; See saved response for additional information');
+			return null;
+		}
+		$reader->read();
+		$list = t3lib_div::makeInstance('tx_icslibnavitia_NodeList', 'tx_icslibnavitia_StopArea');
+		while ($reader->nodeType != XMLReader::END_ELEMENT) {
+			if ($reader->nodeType == XMLReader::ELEMENT) {
+				switch ($reader->name) {
+					case 'Params':
+						$this->SkipChildren($reader);
+						break;
+					case 'StopAreaList':
+						tx_icslibnavitia_Node::ReadList($reader, $list, array('StopArea' => 'tx_icslibnavitia_StopArea'));
+						break;
+					case 'PagerInfo':
+						$this->SkipChildren($reader);
+						break;
+					default:
+						$this->SkipChildren($reader);
+				}
+			}
+			$reader->read();
+		}
+		return $list;
+	}
 	
 	/**
 	 * Query the DepartureBoard API function.
@@ -756,7 +802,8 @@ class tx_icslibnavitia_APIService {
 	public function getBinaryCriteria(array $modeTypeExternalCodes, array $flags = array()) {
 		static $availableFlags = null;
 		if ($availableFlags == null) {
-			$availableFlags = (new ReflectionClass(get_class($this)))->getConstants();
+			$reflection = new ReflectionClass(get_class($this));
+			$availableFlags = $reflexion->getConstants();
 			foreach (array_keys($availableFlags) as $key) {
 				if (!preg_match('/^CRITERIA/', $key)) unset($availableFlags[$key]);
 			}
